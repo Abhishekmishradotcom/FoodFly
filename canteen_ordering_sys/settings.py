5564bbd57bea13c60a2710c2b300505f52adea2e
Abhishekmishradotcom <abhimishra6542@gmail.com>
2026-03-02 22:32:28 +0530

    fix one bug

--- canteen_ordering_sys/settings.py
+++ canteen_ordering_sys/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = ['your-app-name.onrender.com']
+# ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
